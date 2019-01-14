87d26584e8815df61e23c99b4291a468e9479e0c
Ibrahim Butt <ibrahimbutt@me.com>
2019-01-14 17:06:36 +0000

    Oystercard allows top up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe Oystercard do
   end
 
   it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  describe '#top_up' do
+    it 'tops up' do
+      expect(subject.top_up(5)).to eq 8
+    end
+  end
 end
