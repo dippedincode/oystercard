ba8ef2b15a5e2e61d9cb2503f813571ab2bec88c
Diptis Halder <diptis.halder@gmail.com>
2019-01-18 14:39:12 +0000

    v14.2 Added #start_journey and #stop_journey methods to Journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -3,8 +3,14 @@ class Journey
   def entry_station
   end 
 
-
   def exit_station
   end 
+
+  def start_journey
+  end 
+
+  def stop_journey
+  end 
+
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -6,5 +6,9 @@ describe Journey do
 
   it { is_expected.to respond_to :entry_station }
   it { is_expected.to respond_to :exit_station }
+  it { is_expected.to respond_to :start_journey }
+  it { is_expected.to respond_to :stop_journey }
+  it { is_expected.to respond_to :calculate_fare }
+  it { is_expected.to respond_to :confirm_journey_complete? }
   
 end
